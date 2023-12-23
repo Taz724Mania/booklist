548e87d9f6d7f7e908cf8badb603ad93ca378c1a
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-23 10:28:28 -0500

    adding delte option

--- server.js
+++ server.js
@@ -1,43 +1,99 @@
-// DEPENDENCIES
-require("dotenv").config();
-require("./config/db.js");
-const express = require("express");
-const morgan = require("morgan");
-const Book = require('./models/Book.js')
+/**
+ * Dependencies
+ */
+require("dotenv").config() // this is how we make use of our .env variables
+require("./config/db") // bring in our db config
+const express = require("express")
+const morgan = require("morgan") // logger
+const methodOverride = require('method-override')
+
 const app = express();
-const { PORT = 3013 } = process.env;
+const { PORT = 3013 } = process.env; 
 
-// MIDDLE WARE
-app.use(morgan("dev")); // logging
-app.use(express.urlencoded({ extended: false }));
-// how we get access to the req.body
+// Bring in our model
+const Book = require("./models/Book")
 
-// ROUTES
-// Index - Get render all books
+/**
+ * Middleware
+ */
+// app.use((req, res, next) => {
+//     console.log("this is middleware")
+//     next()
+// })
+app.use(morgan("dev")) // logging
+app.use(express.urlencoded({ extended: true })) // body parser this is how we get access to req.body
+app.use(methodOverride("_method")) //lets us use DELETE PUT HTTP verbs
 
-// New - Get for the form to create a new book
+
+/**
+ * Routes & Router
+ */
+
+// Index - GET render all of the books
+app.get("/books", async (req, res) => {
+    // find all of the books
+    let books = await Book.find({})
+
+    // render all of the books to index.ejs
+    res.render("index.ejs", {
+        books: books.reverse()
+    })
+})
+
+// New - GET for the form to create a new book
 app.get("/books/new", (req, res) => {
-  res.render("new.ejs");
-});
+    // render the create form
+    res.render("new.ejs")
+})
 
-// Create - Post
-app.post("/books", async (req, res) => {
-  try {
-    if (req.body.completed === "on") {
-      // if checked
-      req.body.completed = true;
-    } else {
-      // not checked
-      req.body.completed = false;
+//DELETE
+app.delete("/books/:id", (req, res) => {
+    try {
+           //Find a book and then delete it
+    let deletedBook = Book.findByIdAndDelete(req.params.id)
+    //redirect back to the index
+    res.redirect("/books")
+    } catch (error) {
+        res.status(500).send('something went wrong')
     }
-    let newBook = await Book.create(req.body);
-    console.log(newBook)
-    res.send(newBook);
-  } catch (err) {
-    console.log(err);
-  }
-});
-// Show
+ 
+})
 
-// LISTENER
-app.listen(PORT, () => console.log(`listening to the sound of ${PORT}`));
+
+//UPDATE
+
+// Create - POST
+app.post("/books", async (req, res) => {
+    try {
+        if (req.body.completed === "on") {
+            // if checked
+            req.body.completed = true
+        } else {
+            // if not checked
+            req.body.completed = false
+        }
+    
+        let newBook = await Book.create(req.body)
+        res.redirect("/books")
+
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+//EDIT
+
+// Show - GET rendering only one book
+app.get("/books/:id", async (req, res) => {
+  // find book by _id
+  let foundBook = await Book.findById(req.params.id) //the request params object
+
+  //render show.ejs w/ the foundbook
+  res.render("show.ejs", {
+    book: foundBook
+  })
+})
+/**
+ * Server listener
+ */
+app.listen(PORT, () => console.log(`Listening to the sounds of ${PORT}`))
